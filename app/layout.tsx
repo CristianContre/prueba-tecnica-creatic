4617c640e8e34a1e262541a729776f17f38daf2d
CrCon <contreras950531@gmail.com>
2023-07-19 22:00:43 -0500

    delete export metadata to build in vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,10 @@
 
 import PageContent from "@/components/PageContent";
 import { store } from "@/store/store";
-import type { Metadata } from "next";
 import { Provider } from "react-redux";
 import Header from "./Header";
 import SocialMedia from "./SocialMedia";
 import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "CDT CreaTIC",
-  description: "Centro de desarrollo tecnológico CreaTIC",
-};
 
 export default function RootLayout({
   children,
